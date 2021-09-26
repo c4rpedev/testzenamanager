2bd7f4ef09201270884be86d86840beb38d33d23
alexandromag24 <34452436+alexandromag24@users.noreply.github.com>
2021-09-26 18:36:03 -0500

    modified enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,23 +4,8 @@
 
 export const environment = {
   production: true,
-  PARSE_APP_ID: 'dbbuttymanag',
-  PARSE_JS_KEY: '88030911379',
-  serverURL: 'https://dbbuttymanag.herokuapp.com/parse'
+  PARSE_APP_ID: 'zenabd1942',
+  PARSE_JS_KEY: '20211942',
+  serverURL: 'https://zenaparse.herokuapp.com/parse'
 };
-
-// -- DEV
-// export const environment = {
-//   production: false,
-//   PARSE_APP_ID: 'developparse1942',
-//   PARSE_JS_KEY: '741852963741',
-//   serverURL: 'http://developparse.herokuapp.com/parse'
-// };
-//-- old
-// export const environment = {
-//   production: false,
-//   PARSE_APP_ID: 'vH5Y2pQQTnE8odu7xeMKMzviCtFuPHQAvQogW4GI',
-//   PARSE_JS_KEY: 'VU2CPIUqrowVntxq4fNZwO7jd94evbNoyethvDvK',
-//   serverURL: 'https://parseapi.back4app.com'
-// };
 
