df599887e118462b18f4cf96be46576aa833fadd
Jorge Alonso Podadera <yoyi1239707@gmail.com>
2021-10-12 10:59:20 -0400

    environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  PARSE_APP_ID: 'zenabd1942',
-  PARSE_JS_KEY: '20211942',
-  serverURL: 'https://zenaparse.herokuapp.com/parse'
+  PARSE_APP_ID: 'developparse1942',
+  PARSE_JS_KEY: '741852963741',
+  serverURL: 'https://developparse.herokuapp.com/parse'
 };
 
